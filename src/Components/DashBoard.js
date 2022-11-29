c3e6a6590bbb643e3ccbaded6207bef18d05e7be
divakarsingh16 <61109071+divakarsingh16@users.noreply.github.com>
2022-11-29 18:32:40 +0530

    Add files via upload

---
+++ src/Components/DashBoard.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+const DashBoard = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-8">
+          <h2>Import your data</h2>
+          <p>Import your data from ............................</p>
+        </div>
+        <div className="col-4">
+          <input placeholder="search" type="search" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <ul className="ul-1">
+            <li>category</li>
+            <li>all</li>
+            <li>mostpopular</li>
+            <li>category</li>
+            <li>all</li>
+            <li>mostpopular</li>
+            <li>category</li>
+            <li>all</li>
+            <li>mostpopular</li>
+            <li>category</li>
+            <li>all</li>
+            <li>mostpopular</li>
+          </ul>
+        </div>
+        <div className="col-9">
+          <ul className="ul-2">
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+            <li>
+              <img alt="smallImages" src="Image/logo.png" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
